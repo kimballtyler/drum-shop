713c4d597d9b2147947256fa80e3fbd47ff20574
kimballtyler <kimballstyler@gmail.com>
2020-09-16 16:12:25 -0400

    Completed view products front

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function ProductListItem(props) {
+  const price = props.product.price.toString();
+  let dollars, cents;
+  if (price.length === 4) {
+    dollars = price.substr(0, 2);
+    cents = price.substr(2);
+  } else {
+    dollars = price.substr(0, 1);
+    cents = price.substr(1, 2);
+  }
+
+  return (
+    <div className="col-4 product-card d-flex align-items-stretch">
+      <div className="card">
+        <img height="200" src={props.product.image} className="card-img-top product-image" alt={props.product.name} />
+        <div className="card-body">
+          <h5 className="card-title">{props.product.name}</h5>
+          <p className="card-text"><small className="text-secondary">${dollars}.{cents}</small></p>
+          <p className="card-text">{props.product.shortDescription}</p>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default ProductListItem;
