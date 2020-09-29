8c0b0c7d8ecafda4ddf0e8d6f4bacff42aa783a3
kimballtyler <kimballstyler@gmail.com>
2020-09-28 23:44:18 -0400

    changed to drum shop, mobile responsivness, and personal info warning

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,8 +5,8 @@ function Header(props) {
   return (
     <nav className="navbar navbar-light bg-dark">
       <div className="navbar-brand text-white ml-5" href="#">
-        <img src="https://www.creativefabrica.com/wp-content/uploads/2019/03/Monogram-WS-Logo-Design-by-Greenlines-Studios.jpg" width="30" height="30" className="d-inline-block align-top mr-1"/>
-          Wicked Sales
+        <img src="/images/drum-favicon.png" width="30" height="30" className="d-inline-block align-top mr-1"/>
+          Drum Shop
       </div>
       <div onClick={() => props.setView('cart', {})} className="text-white mr-5 cart-div">
         <span className="mr-2">{`${props.cartItemCount} ${cartText}`}</span>
